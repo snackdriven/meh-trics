9029b8d5790f8a1c06b3f785ee598443f04d464d
kayla <78115189+snackdriven@users.noreply.github.com>
2025-06-06 22:40:35 -0500

    docs: note new offline PWA support (#134)

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { registerSW } from "virtual:pwa-register";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+registerSW({ immediate: true });
+
+const rootEl = document.getElementById("root") as HTMLElement;
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
